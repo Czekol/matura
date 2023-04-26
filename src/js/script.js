493c52cde2b83bc8216477fad6cc2f9a91e025e4
Bartosz <bartoszweb8@gmail.com>
2023-04-26 23:13:56 +0200

    school update 26.04 and a little bit code form home

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,34 @@
 // HAMBURGER MENU
-const burgerBtn = document.querySelector('.nav__burger-btn');
-const mobileLinks = document.querySelector('.nav__mobile-links');
-const navShadow = document.querySelector('.nav__shadow');
+const burgerBtn = document.querySelector('.nav__burger-btn')
+const mobileLinks = document.querySelector('.nav__mobile-links')
+const navShadow = document.querySelector('.nav__shadow')
+// ALL BTNS IN OFFER
+const offerBtns = document.querySelectorAll('.offers__card-btn')
 // CURRENT YEAR IN FOOTER
-const footerYear = document.querySelector('.footer__current-year');
+const footerYear = document.querySelector('.footer__current-year')
 
 const navToggle = () => {
-	mobileLinks.classList.toggle('show-links');
-	navShadow.classList.toggle('show-shadow');
-	burgerBtn.classList.toggle('rotate-burger-btn');
-};
+	mobileLinks.classList.toggle('show-links')
+	navShadow.classList.toggle('show-shadow')
+	burgerBtn.classList.toggle('rotate-burger-btn')
+}
 
-// --- ---  
+// --- ---
+
+const selectingOffer = e => {
+	offerBtns.forEach(btn => btn.classList.remove('offers__card-btn--cta'))
+	e.target.classList.add('offers__card-btn--cta')
+}
+
+// --- ---
 
 const handleCurrentYear = () => {
-	const currentYear = new Date().getFullYear();
-    footerYear.textContent = currentYear
-};
+	const currentYear = new Date().getFullYear()
+	footerYear.textContent = currentYear
+}
 
-handleCurrentYear();
+handleCurrentYear()
 
-burgerBtn.addEventListener('click', navToggle);
-navShadow.addEventListener('click', navToggle);
+burgerBtn.addEventListener('click', navToggle)
+navShadow.addEventListener('click', navToggle)
+offerBtns.forEach(btn => btn.addEventListener('click', selectingOffer))
