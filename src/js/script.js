699b52c80afc7f2088664c4ccb4219aca365f48d
Bartosz <bartoszweb8@gmail.com>
2023-04-26 02:53:06 +0200

    almost finished footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,24 @@
-const burgerBtn = document.querySelector('.nav__burger-btn')
-const mobileLinks = document.querySelector('.nav__mobile-links')
-const navShadow = document.querySelector('.nav__shadow')
+// HAMBURGER MENU
+const burgerBtn = document.querySelector('.nav__burger-btn');
+const mobileLinks = document.querySelector('.nav__mobile-links');
+const navShadow = document.querySelector('.nav__shadow');
+// CURRENT YEAR IN FOOTER
+const footerYear = document.querySelector('.footer__current-year');
 
 const navToggle = () => {
-    mobileLinks.classList.toggle('show-links')
-    navShadow.classList.toggle('show-shadow')
-    burgerBtn.classList.toggle('rotate-burger-btn')
-}
-burgerBtn.addEventListener('click',navToggle)
-navShadow.addEventListener('click',navToggle)
+	mobileLinks.classList.toggle('show-links');
+	navShadow.classList.toggle('show-shadow');
+	burgerBtn.classList.toggle('rotate-burger-btn');
+};
+
+// --- ---  
+
+const handleCurrentYear = () => {
+	const currentYear = new Date().getFullYear();
+    footerYear.textContent = currentYear
+};
+
+handleCurrentYear();
+
+burgerBtn.addEventListener('click', navToggle);
+navShadow.addEventListener('click', navToggle);
