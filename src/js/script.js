b96ef19b73e5698178f059a2f62f3646d31025ea
Bartosz <bartoszweb8@gmail.com>
2023-04-29 03:04:12 +0200

    added pof on nav and code correctly worinkg mobile nav and hooks

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,39 @@
 // HAMBURGER MENU
-const burgerBtn = document.querySelector('.nav__burger-btn')
-const mobileLinks = document.querySelector('.nav__mobile-links')
-const navShadow = document.querySelector('.nav__shadow')
+const body = document.querySelector('body');
+const burgerBtn = document.querySelector('.nav__burger-btn');
+const navMobile = document.querySelector('.nav__mobile-links');
+const mobileNavLinks = document.querySelectorAll('.nav__mobile-links-link');
+const navShadow = document.querySelector('.nav__shadow');
 // ALL BTNS IN OFFER
-const offerBtns = document.querySelectorAll('.offers__card-btn')
+const offerBtns = document.querySelectorAll('.offers__card-btn');
 // CURRENT YEAR IN FOOTER
-const footerYear = document.querySelector('.footer__current-year')
+const footerYear = document.querySelector('.footer__current-year');
 
 const navToggle = () => {
-	mobileLinks.classList.toggle('show-links')
-	navShadow.classList.toggle('show-shadow')
-	burgerBtn.classList.toggle('rotate-burger-btn')
-}
+	navMobile.classList.toggle('show-links');
+	navShadow.classList.toggle('show-shadow');
+	burgerBtn.classList.toggle('rotate-burger-btn');
+	body.classList.toggle('stop-scrolling');
 
+};
+
+mobileNavLinks.forEach(link => link.addEventListener('click', navToggle))
 // --- ---
 
 const selectingOffer = e => {
-	offerBtns.forEach(btn => btn.classList.remove('offers__card-btn--cta'))
-	e.target.classList.add('offers__card-btn--cta')
-}
+	offerBtns.forEach(btn => btn.classList.remove('offers__card-btn--cta'));
+	e.target.classList.add('offers__card-btn--cta');
+};
 
 // --- ---
 
 const handleCurrentYear = () => {
-	const currentYear = new Date().getFullYear()
-	footerYear.textContent = currentYear
-}
+	const currentYear = new Date().getFullYear();
+	footerYear.textContent = currentYear;
+};
 
-handleCurrentYear()
+handleCurrentYear();
 
-burgerBtn.addEventListener('click', navToggle)
-navShadow.addEventListener('click', navToggle)
-offerBtns.forEach(btn => btn.addEventListener('click', selectingOffer))
+burgerBtn.addEventListener('click', navToggle);
+navShadow.addEventListener('click', navToggle);
+offerBtns.forEach(btn => btn.addEventListener('click', selectingOffer));
